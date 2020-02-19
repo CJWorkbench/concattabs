a83f676206f0a85f5d5ce490ecbfc51279bb4caf
Georgios Pitsiladis <g.v.pitsiladis@gmail.com>
2020-02-19 11:04:07 +0200

    Add cjwmodule in setup.py

--- setup.py
+++ setup.py
@@ -11,5 +11,5 @@ setup(
     url="https://github.com/CJWorkbench/concattabs",
     packages=[""],
     py_modules=["concattabs"],
-    install_requires=["pandas==0.25.0"],
+    install_requires=["pandas==0.25.0", "cjwmodule>=1.4.0"],
 )
